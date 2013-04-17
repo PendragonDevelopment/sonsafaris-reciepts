cd94491b4f2e41bbbcb6a5bcccfb7d0182421ae7
Jordan K. Burke <me@jordankburke.com>
2013-04-16 21:08:31 -0400

    Added seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,6 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+user2 = User.find_or_create_by_email :name => "Fledge Fiamingo", :email => "fledge@sonsafaris.com", :password => "changeme", :password_confirmation => "changeme"
+puts 'user: ' << user.name
+user.add_role :admin
