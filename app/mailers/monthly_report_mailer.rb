e38e0c734cbcd24af4acd1717810252c4840bd6e
Jordan K. Burke <me@jordankburke.com>
2013-05-04 20:51:20 -0400

    Closes #3.

--- app/mailers/monthly_report_mailer.rb
+++ app/mailers/monthly_report_mailer.rb
@@ -10,7 +10,10 @@ class MonthlyReportMailer < MandrillMailer::TemplateMailer
       'FNAME' => donor.first_name,
       'AMOUNT' => donation.amount,
       'DONDATE' => donation.donation_date,
-      'CHNUMBER' => donation.details
+      'CHNUMBER' => donation.details,
+      'LIST:COMPANY' => "Son Safaris",
+      'LIST:DESCRIPTION' => "You are receiving this email because you opted in at SonSafaris.com",
+      'HTML:LIST_ADDRESS_HTML' => "Son Safaris<br />P.O. Box 48476<br />Athens, Georgia 30604"
     },
     headers: {"X-MC-SigningDomain" => "http://sonsafaris.com"}
   end
